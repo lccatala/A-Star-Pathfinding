e1d9b8954bf7a22d5ad6d02f57513301b7a94693
lccatala <lccm1@alu.ua.es>
2018-09-21 17:11:26 +0200

    First Commit

---
+++ core/src/AEstrella/AEstrella.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AEstrella;
+
+
+/**
+ *
+ * @author mirse
+ */
+public class AEstrella {
+ 
+    //Mundo sobre el que se debe calcular A*
+    Mundo mundo;
+    
+    //Camino
+    public char camino[][];
+    
+    //Casillas expandidas
+    int camino_expandido[][];
+    
+    //Número de nodos expandidos
+    int expandidos;
+    
+    //Coste del camino
+    float coste_total;
+    
+    public AEstrella(){
+        expandidos = 0;
+        mundo = new Mundo();
+    }
+    
+    public AEstrella(Mundo m){
+        //Copia el mundo que le llega por parámetro
+        mundo = new Mundo(m);
+        camino = new char[m.tamanyo_y][m.tamanyo_x];
+        camino_expandido = new int[m.tamanyo_y][m.tamanyo_x];
+        expandidos = 0;
+        
+        //Inicializa las variables camino y camino_expandidos donde el A* debe incluir el resultado
+            for(int i=0;i<m.tamanyo_x;i++)
+                for(int j=0;j<m.tamanyo_y;j++){
+                    camino[j][i] = '.';
+                    camino_expandido[j][i] = -1;
+                }
+    }
+    
+    //Calcula el A*
+    public int CalcularAEstrella(){
+
+        boolean encontrado = false;
+        int result = -1;
+        
+        //AQUÍ ES DONDE SE DEBE IMPLEMENTAR A*
+       
+
+        //Si ha encontrado la solución, es decir, el camino, muestra las matrices camino y camino_expandidos y el número de nodos expandidos
+        if(encontrado){
+            //Mostrar las soluciones
+            System.out.println("Camino");
+            mostrarCamino();
+
+            System.out.println("Camino explorado");
+            mostrarCaminoExpandido();
+            
+            System.out.println("Nodos expandidos: "+expandidos);
+        }
+
+        return result;
+    }
+    
+
+    
+    
+   
+    
+    //Muestra la matriz que contendrá el camino después de calcular A*
+    public void mostrarCamino(){
+        for (int i=0; i<mundo.tamanyo_y; i++){
+            if(i%2==0)
+                System.out.print(" ");
+            for(int j=0;j<mundo.tamanyo_x; j++){
+                System.out.print(camino[i][j]+" ");
+            }
+            System.out.println();   
+        }
+    }
+    
+    //Muestra la matriz que contendrá el orden de los nodos expandidos después de calcular A*
+    public void mostrarCaminoExpandido(){
+        for (int i=0; i<mundo.tamanyo_y; i++){
+            if(i%2==0)
+                    System.out.print(" ");
+            for(int j=0;j<mundo.tamanyo_x; j++){
+                if(camino_expandido[i][j]>-1 && camino_expandido[i][j]<10)
+                    System.out.print(" ");
+                System.out.print(camino_expandido[i][j]+" ");
+            }
+            System.out.println();   
+        }
+    }
+    
+    public void reiniciarAEstrella(Mundo m){
+        //Copia el mundo que le llega por parámetro
+        mundo = new Mundo(m);
+        camino = new char[m.tamanyo_y][m.tamanyo_x];
+        camino_expandido = new int[m.tamanyo_y][m.tamanyo_x];
+        expandidos = 0;
+        
+        //Inicializa las variables camino y camino_expandidos donde el A* debe incluir el resultado
+            for(int i=0;i<m.tamanyo_x;i++)
+                for(int j=0;j<m.tamanyo_y;j++){
+                    camino[j][i] = '.';
+                    camino_expandido[j][i] = -1;
+                }
+    }
+    
+    public float getCosteTotal(){
+        return coste_total;
+    }
+}
+
+
